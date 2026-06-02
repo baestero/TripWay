df90e780391c3aa0f7e6a5f4c510b32d1bcf5b8a
baestero <leonardobaestero@gmail.com>
2026-06-02 16:37:56 -0300

    debug

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,23 +1,30 @@
 #!/bin/sh
 set -e
 
-cat > config/app_local.php << 'EOF'
+cat > /var/www/html/config/app_local.php << 'EOF'
 <?php
 return [
     'debug' => true,
     'Security' => [
-        'salt' => env('SECURITY_SALT', 'algum-salt-aqui-qualquer-string-longa'),
+        'salt' => env('SECURITY_SALT', 'algum-salt-longo-aqui'),
     ],
     'Datasources' => [
-       'default' => [
-        'url' => env('DATABASE_URL', null),
+        'default' => [
+            'driver' => 'Cake\Database\Driver\Postgres',
+            'host' => env('DB_HOST', 'localhost'),
+            'username' => env('DB_USER', ''),
+            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', ''),
+            'port' => env('DB_PORT', '5432'),
+            'encoding' => 'utf8',
+            'timezone' => 'UTC',
+            'cacheMetadata' => true,
         ],
     ],
 ];
 EOF
 
 echo "==> app_local.php criado!"
-echo "==> DATABASE_URL: $DATABASE_URL"
 echo "==> Running migrations..."
 bin/cake migrations migrate -c default || true
 
